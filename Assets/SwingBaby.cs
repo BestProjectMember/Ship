096b86586eef6c71d92c8c8af2b08fe4d8cca40c
LukasJOfficial <54264562+LukasJOfficial@users.noreply.github.com>
2019-12-06 10:32:46 +0100

    all the files

---
+++ Assets/SwingBaby.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingBaby : MonoBehaviour
+{
+    public float timeframe;
+    public Transform start, end;
+    private bool up = false;
+
+    void Start()
+    {
+
+    }
+    private void Update()
+    {
+        if (up)
+        {
+            transform.rotation = Quaternion.Lerp(start.rotation, end.rotation, Time.time * timeframe);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(end.rotation, start.rotation, Time.time * timeframe);
+        }
+        if (transform.rotation == start.rotation)
+        {
+            up = true;
+        } else if (transform.rotation == end.rotation)
+        {
+            up = false;
+        }
+    }
+
+}
